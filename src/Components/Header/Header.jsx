c40223a852311b4a9c21e9167e37e9d011f5cb72
Dimitri Hector <joshuahector86@gmail.com>
2023-08-02 18:13:35 -0400

    header section content

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,7 +1,15 @@
 import "./Header.scss";
+import logo from "../../assets/logo.jpg";
+import Typed from "typed.js";
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <div className="header">
+      <img src={logo} alt="logo enlarged" />
+
+      <div className="dancing-dev-title">The Dancing Developer</div>
+    </div>
+  );
 }
 
 export default Header;
