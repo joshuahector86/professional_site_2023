ff385fd1f41776ddb6775d355c00e1c70e1dba51
Dimitri Hector <joshuahector86@gmail.com>
2023-08-02 19:08:58 -0400

    services content

--- src/Components/Services/Services.jsx
+++ src/Components/Services/Services.jsx
@@ -1,7 +1,43 @@
 import "./Services.scss";
 
+import { RiComputerLine } from "react-icons/ri";
+
+import { TbPodium } from "react-icons/tb";
+
+import { FaFlipboard } from "react-icons/fa";
+
+import { MdOutlineScience } from "react-icons/md";
+
 function Services() {
-  return <div>Services</div>;
+  return (
+    <div className="services">
+      <div className="service-box">
+        <div className="icon-circle">
+          <RiComputerLine />
+        </div>
+        <p>Front End</p>
+        <p>Development</p>
+      </div>
+      <div className="service-box">
+        <div className="icon-circle">
+          <TbPodium />
+        </div>
+        Public Speaking
+      </div>
+      <div className="service-box">
+        <div className="icon-circle">
+          <FaFlipboard />
+        </div>
+        UI/UX Design
+      </div>
+      <div className="service-box">
+        <div className="icon-circle">
+          <MdOutlineScience />
+        </div>
+        Innovation
+      </div>
+    </div>
+  );
 }
 
 export default Services;
