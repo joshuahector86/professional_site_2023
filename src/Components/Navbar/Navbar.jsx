fc03e4133392c730490504ccccfd170078df4948
Dimitri Hector <joshuahector86@gmail.com>
2023-08-03 14:26:27 -0400

    successfully reseting a variable on scroll idle

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -7,39 +7,65 @@ function Navbar() {
   // Logic for showing and hiding the navbar on scroll
   const [scrollUp, setScrollUp] = useState(true);
   const [prevScrollPos, setPrevScrollPos] = useState(0);
+  const [clickOnWindow, setClickOnWindow] = useState(false);
+  // const [isScrolling, setIsScrolling] = 0;
+  let isScrolling;
+  let myVariable = 0;
   useEffect(() => {
     const handleScroll = () => {
       const currentScrollPos = window.scrollY;
       setScrollUp(prevScrollPos > currentScrollPos);
       setPrevScrollPos(currentScrollPos);
-      setIsIdle(false);
+      console.log("hello i am here");
     };
     window.addEventListener("scroll", handleScroll);
+    window.addEventListener("scroll", startIdleTimer);
     return () => {
       window.removeEventListener("scroll", handleScroll);
     };
-  }, [prevScrollPos]); // useEffect stops the infinite looping
+  }, [prevScrollPos]); // HANDLE SCROLL FOR SETTING SCROLLUP TO TRUE OR FALSE
+
+  const handleWindowClick = () => {
+    const state = !clickOnWindow;
+    setClickOnWindow(state);
+    setTimeout(setClickOnWindow(false), 2000);
+    console.log("handle click was reached");
+  }; // HANDLE CLICK ON NAVBAR ELEMENT TO CHANGE CLICKONWINDOW TO TRUE OR FALSE
+
+  function onScrollIdle() {
+    // Reset your variable or do any other action you want here
+    myVariable = 0;
+    console.log("Scrolling is now idle. Variable has been reset to 0.");
+  }
+  function startIdleTimer() {
+    clearTimeout(isScrolling);
+    isScrolling = setTimeout(onScrollIdle, 200);
+  }
 
   return (
-    <div className={scrollUp ? "navbar" : "navbar hidden"}>
+    <div
+      className={`${scrollUp ? "navbar" : "navbar hidden"} ${
+        scrollUp && clickOnWindow ? "navbar hidden" : "navbar"
+      }`}
+    >
       <div className="logo-side">
         <img src={logo} alt="Logo" />
       </div>
       <div className="links">
         <Link smooth={true} to="home" offset={0}>
-          <button>HOME</button>
+          <button onClick={handleWindowClick}>HOME</button>
         </Link>
         <Link smooth={true} to="aboutme" offset={20}>
-          <button>ABOUT ME</button>
+          <button onClick={handleWindowClick}>ABOUT ME</button>
         </Link>
         <Link smooth={true} to="experience" offset={10}>
-          <button>EXPERIENCE</button>
+          <button onClick={handleWindowClick}>EXPERIENCE</button>
         </Link>
-        <Link smooth={true} to="portfolio" offset={20}>
-          <button>PORTFOLIO</button>
+        <Link smooth={true} to="portfolio" offset={-100}>
+          <button onClick={handleWindowClick}>PORTFOLIO</button>
         </Link>
         <Link smooth={true} to="contactme" offset={20}>
-          <button>CONTACT ME</button>
+          <button onClick={handleWindowClick}>CONTACT ME</button>
         </Link>
       </div>
     </div>
