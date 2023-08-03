7f77142ee07cf62b081e70395f7e09196e6a11ff
Dimitri Hector <joshuahector86@gmail.com>
2023-08-02 21:42:39 -0400

    contact me content

--- src/Components/ContactMe/ContactMe.jsx
+++ src/Components/ContactMe/ContactMe.jsx
@@ -1,7 +1,27 @@
 import "./ContactMe.scss";
 
 function ContactMe() {
-  return <div>ContactMe</div>;
+  return (
+    <div className="contact-me">
+      <div className="contact-me-title">Contact Me</div>
+      <div className="contact-me-form-container">
+        <div className="form-left-side">
+          <input type="text" placeholder="Name" />
+          <input type="text" placeholder="Phone Number" />
+          <input type="text" placeholder="Email Address" />
+        </div>
+        <div className="form-right-side">
+          <input type="text" placeholder="Email Subject" />
+          <input
+            className="message-input"
+            type="text"
+            placeholder="Email Message"
+          />
+        </div>
+      </div>
+      <button>Submit</button>
+    </div>
+  );
 }
 
 export default ContactMe;
