ee256cd0f7455fb2c163ef50cc217d0e8126cae3
Dimitri Hector <joshuahector86@gmail.com>
2023-08-03 00:13:23 -0400

    resizing fonts and spacing the page

--- src/Components/Experience/Experience.jsx
+++ src/Components/Experience/Experience.jsx
@@ -13,15 +13,16 @@ function Experience() {
       <div className="experience-inner-container">
         <div className="experience-title">Experience</div>
         <div className="experience-sub-title1">Professional</div>
-
-        <div className="experience-left-side">
-          <CiscoSoftwareEngineer />
-        </div>
-        <div className="experience-right-side">
-          <RoboticsResearcher />
-        </div>
-        <div className="experience-left-side">
-          <UndergraduateResearcherContent />
+        <div className="experience-holder">
+          <div className="experience-tab">
+            <CiscoSoftwareEngineer />
+          </div>
+          <div className="experience-tab">
+            <RoboticsResearcher />
+          </div>
+          <div className="experience-tab">
+            <UndergraduateResearcherContent />
+          </div>
         </div>
         <div className="experience-sub-title2">Education</div>
         <div className="education-container">
