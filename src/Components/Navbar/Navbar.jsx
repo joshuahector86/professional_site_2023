518bd21be7060ccb13e0b9030c8ead8508d221af
Dimitri Hector <joshuahector86@gmail.com>
2023-08-03 14:53:36 -0400

    reversing previous logic back to just navbar dissapear on scroll up

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -9,17 +9,14 @@ function Navbar() {
   const [prevScrollPos, setPrevScrollPos] = useState(0);
   const [clickOnWindow, setClickOnWindow] = useState(false);
   // const [isScrolling, setIsScrolling] = 0;
-  let isScrolling;
-  let myVariable = 0;
+
   useEffect(() => {
     const handleScroll = () => {
       const currentScrollPos = window.scrollY;
       setScrollUp(prevScrollPos > currentScrollPos);
       setPrevScrollPos(currentScrollPos);
-      console.log("hello i am here");
     };
     window.addEventListener("scroll", handleScroll);
-    window.addEventListener("scroll", startIdleTimer);
     return () => {
       window.removeEventListener("scroll", handleScroll);
     };
@@ -28,26 +25,10 @@ function Navbar() {
   const handleWindowClick = () => {
     const state = !clickOnWindow;
     setClickOnWindow(state);
-    setTimeout(setClickOnWindow(false), 2000);
-    console.log("handle click was reached");
   }; // HANDLE CLICK ON NAVBAR ELEMENT TO CHANGE CLICKONWINDOW TO TRUE OR FALSE
 
-  function onScrollIdle() {
-    // Reset your variable or do any other action you want here
-    myVariable = 0;
-    console.log("Scrolling is now idle. Variable has been reset to 0.");
-  }
-  function startIdleTimer() {
-    clearTimeout(isScrolling);
-    isScrolling = setTimeout(onScrollIdle, 200);
-  }
-
   return (
-    <div
-      className={`${scrollUp ? "navbar" : "navbar hidden"} ${
-        scrollUp && clickOnWindow ? "navbar hidden" : "navbar"
-      }`}
-    >
+    <div className={`${scrollUp ? "navbar" : "navbar hidden"} `}>
       <div className="logo-side">
         <img src={logo} alt="Logo" />
       </div>
