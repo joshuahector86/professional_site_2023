da12bf8582b934d56be7efbb1d10b4665bca562d
Dimitri Hector <joshuahector86@gmail.com>
2023-08-04 16:19:51 -0400

    switching to hamburger menu for navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -3,52 +3,45 @@ import { Link } from "react-scroll";
 import { useEffect, useState } from "react";
 import logo from "../../assets/logo.jpg";
 
+import { FiMenu } from "react-icons/fi";
+import { AiOutlineClose } from "react-icons/ai";
+
 function Navbar() {
-  // Logic for showing and hiding the navbar on scroll
-  const [scrollUp, setScrollUp] = useState(true);
-  const [prevScrollPos, setPrevScrollPos] = useState(0);
-  const [clickOnWindow, setClickOnWindow] = useState(false);
-  // const [isScrolling, setIsScrolling] = 0;
-
-  useEffect(() => {
-    const handleScroll = () => {
-      const currentScrollPos = window.scrollY;
-      setScrollUp(prevScrollPos > currentScrollPos);
-      setPrevScrollPos(currentScrollPos);
-    };
-    window.addEventListener("scroll", handleScroll);
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, [prevScrollPos]); // HANDLE SCROLL FOR SETTING SCROLLUP TO TRUE OR FALSE
-
-  const handleWindowClick = () => {
-    const state = !clickOnWindow;
-    setClickOnWindow(state);
-  }; // HANDLE CLICK ON NAVBAR ELEMENT TO CHANGE CLICKONWINDOW TO TRUE OR FALSE
+  const [menuOpen, setMenuOpen] = useState(true);
+  function handleClick() {
+    setMenuOpen(!menuOpen);
+    console.log(menuOpen);
+  }
 
   return (
-    <div className={`${scrollUp ? "navbar" : "navbar hidden"} `}>
+    <div className="navbar">
       <div className="logo-side">
         <img src={logo} alt="Logo" />
       </div>
-      <div className="links">
-        <Link smooth={true} to="home" offset={0}>
-          <button onClick={handleWindowClick}>HOME</button>
-        </Link>
-        <Link smooth={true} to="aboutme" offset={20}>
-          <button onClick={handleWindowClick}>ABOUT ME</button>
-        </Link>
-        <Link smooth={true} to="experience" offset={10}>
-          <button onClick={handleWindowClick}>EXPERIENCE</button>
-        </Link>
-        <Link smooth={true} to="portfolio" offset={-100}>
-          <button onClick={handleWindowClick}>PORTFOLIO</button>
-        </Link>
-        <Link smooth={true} to="contactme" offset={20}>
-          <button onClick={handleWindowClick}>CONTACT ME</button>
-        </Link>
+      <div className="toggler" onClick={handleClick}>
+        {menuOpen ? <AiOutlineClose /> : <FiMenu />}
       </div>
+      {menuOpen ? (
+        <div id="navbar-content" className={`links`}>
+          <Link smooth={true} to="home" offset={0}>
+            <button>HOME</button>
+          </Link>
+          <Link smooth={true} to="aboutme" offset={20}>
+            <button>ABOUT ME</button>
+          </Link>
+          <Link smooth={true} to="experience" offset={10}>
+            <button>EXPERIENCE</button>
+          </Link>
+          <Link smooth={true} to="portfolio" offset={-100}>
+            <button>PORTFOLIO</button>
+          </Link>
+          <Link smooth={true} to="contactme" offset={20}>
+            <button>CONTACT ME</button>
+          </Link>
+        </div>
+      ) : (
+        ""
+      )}
     </div>
   );
 }
