a4ffaeb5fcd8fb097afc65d21d351f69b7cfd114
Dimitri Hector <joshuahector86@gmail.com>
2023-08-04 20:04:27 -0400

    Updates

--- src/ZustandStores/WorkInProgressStore/WorkInProgressDialog.jsx
+++
@@ -1,7 +0,0 @@
-import { create } from "zustand";
-
-export const useStoreWIP = create((set) => ({
-  isWIPDialogOpen: false,
-  openWIP: () => set({ isWIPDialogOpen: true }),
-  closeWIP: () => set({ isWIPDialogOpen: false }),
-}));
