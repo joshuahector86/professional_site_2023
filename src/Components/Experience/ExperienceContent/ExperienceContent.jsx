0ff8fe5279a5eefc885c9f0a180d7c1a7d73ab59
Dimitri Hector <joshuahector86@gmail.com>
2024-02-18 15:57:21 -0500

    making changes to experience section

--- src/Components/Experience/ExperienceContent/ExperienceContent.jsx
+++ src/Components/Experience/ExperienceContent/ExperienceContent.jsx
@@ -1,75 +1,24 @@
 import "./ExperienceContent.scss";
 
-function UndergraduateResearcherContent() {
-  return (
-    <div className="experience-content-container">
-      <div className="time-worked">June 2016 - May 2019</div>
-      <div className="bullet-points-title">
-        Ithaca College Undergraduate Researcher
-      </div>
-      <div className="bullet-points">
-        <p>
-          ● Created programs for system requirements of experiments that were
-          done in the lab.
-        </p>
-        <p>
-          ● Developed problem solving skills while designing and executing
-          experiments.
-        </p>
-        <p>● Documented the building and use of a Mobius 3π strip.</p>
-        <p>
-          ● Extensive use of MATLAB and Latex for data analysis and technical
-          writing.
-        </p>
-        <p>
-          ● Managed multiple projects across varying labs of varying
-          disciplines.
-        </p>
-        <p>
-          ● Created publicity for the lab work done by appearing in magazines,
-          giving talks, and creating video documentation.
-        </p>
-      </div>
-    </div>
-  );
-}
-
-function RoboticsResearcher() {
-  return (
-    <div className="experience-content-container">
-      <div className="time-worked">August 2020 - April 2021</div>
-      <div className="bullet-points-title">Autodesk Robotics Researcher</div>
-      <div className="bullet-points">
-        <p>● Designed simulations in Unity for robotic research.</p>
-        <p>
-          ● Developed problem solving skills while designing and executing
-          experiments.
-        </p>
-        <p>● Collected data for analysis and experimentation.</p>
-        <p>
-          ● Developed security for a robotics experiment (a paper was published
-          in the IEEE journal for this work)
-        </p>
-        <p>
-          ● Communicated with a team of professionals to relay work plans,
-          milestones, status, and overall results in presentations.
-        </p>
-      </div>
-    </div>
-  );
-}
-
 function CiscoSoftwareEngineer() {
   return (
     <div className="experience-content-container">
       <div className="time-worked">July 2021 - Present</div>
       <div className="bullet-points-title">Cisco Software Engineer</div>
       <div className="bullet-points">
-        <p>● Software engineer with Cisco’s Innovation sector.</p>
-        <p>● Lead Engineer for UI development of multiple projects.</p>
-        <p>● Creation of new software tools for internal and external use.</p>
-        <p>● Developed SaaS products, and maintained code bases for them.</p>
-        <p>● Developed prototypes for company and shareholder demo.</p>
+        <h3>Lead Engineer for UI Development</h3>
+        <p>
+          ● Spearheaded the development of various UI projects, creating
+          impactful and user-friendly designs.
+        </p>
+        <p>
+          ● Led the migration of legacy systems to a modern tech stack to
+          improve performance and reduce maintenance costs.
+        </p>
+        <p>
+          ● Delegated tasks to team members, fostering a collaborative
+          environment and ensuring project timelines were met.
+        </p>
         <p>
           ● Planned and developed large scale architecture for commercial
           software.
@@ -78,11 +27,68 @@ function CiscoSoftwareEngineer() {
           ● Experience with being a scrum master, UX designer, and software
           marketing.
         </p>
+        <p>
+          <p>
+            ● Led the UI development of Deneb, a project to create and design a
+            security dashboard centered around retail theft prevention. The
+            project made it to the last steps of approval within the company.
+          </p>
+          ● Collaborated with cross-functional teams and potential company
+          customers to design and implement scalable solutions, optimizing
+          reliability, site speed, source code customization, and navigation.
+        </p>
+
+        <h3>Software Tools Development</h3>
+        <p>
+          ● Designed and developed new tools for both internal and external use
+          for the company.
+        </p>
+        <p>
+          ● Contributed to the growth and efficiency of the company by
+          introducing tools that streamlined processes and improved
+          productivity.
+        </p>
+        <p>● Contributed to the company’s UI component and design libraries.</p>
+        <p>
+          ● Contributed to establishing and maintaining UI design standards to
+          create a polished company identity within its many applications.
+        </p>
+
+        <h3>Versatile Roles</h3>
+        <p>
+          ● Acted as a Scrum Master, facilitating agile methodology using
+          software like JIRA.
+        </p>
+        <p>
+          ● Utilized UI design skills to enhance the overall user experience,
+          providing valuable insights to the UX design team.
+        </p>
+        <p>
+          ● Contributed to software marketing initiatives, aligning technical
+          features with customer benefits and creating compelling marketing
+          collateral.
+        </p>
+        <p>
+          ● Mentored junior team members on problem solving strategies and UI
+          best practices in order to promote a culture of continuous learning
+          and collaboration.
+        </p>
+
+        <h3>Architectural Planning</h3>
+        <p>
+          ● Planned and executed large-scale architecture for commercial
+          software, ensuring scalability, and maintainability.
+        </p>
+        <p>
+          ● Developed prototypes for company and shareholder demos to showcase
+          the capabilities of the proposed solutions.
+        </p>
       </div>
     </div>
   );
 }
-function HowardTechResearcher() {
+
+function RoboticsAndSyberSecurityEngineer() {
   return (
     <div className="experience-content-container">
       <div className="time-worked">February 2020 - May 2021</div>
@@ -90,18 +96,26 @@ function HowardTechResearcher() {
         Howard University Tech Researcher
       </div>
       <div className="bullet-points">
+        <h3>Published Research</h3>
         <p>
-          ● Developed cyber security solutions for database and mobile
-          information leaks.
+          ● First Author of published work in the IEEE Journal, contributing to
+          advancements in cyber security measures for robotic systems.
         </p>
-        <p>● Coordinated user expectations with the project owner.</p>
+
+        <h3>Simulation Design</h3>
         <p>
-          ● Directed communication with customers to define requirements for
-          multiple projects.
+          ● Designed testable simulations in the Unity game engine for robotics
+          research, enabling comprehensive experimentation and analysis.
+        </p>
+
+        <h3>Security Engineering</h3>
+        <p>
+          ● Developed and implemented security measures for robotic systems,
+          ensuring the integrity and safety of the machinery.
         </p>
         <p>
-          ● Maintained and improved legacy code bases for future use and
-          integration into modern frameworks.
+          ● Implemented security measures for software systems at Howard
+          University, enhancing overall cybersecurity.
         </p>
       </div>
     </div>
@@ -143,10 +157,8 @@ function BachelorsDegree() {
 }
 
 export {
-  UndergraduateResearcherContent,
-  RoboticsResearcher,
   CiscoSoftwareEngineer,
-  HowardTechResearcher,
+  RoboticsAndSyberSecurityEngineer,
   MastersDegree,
   BachelorsDegree,
 };
